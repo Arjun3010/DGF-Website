4342d579878826ce8247b2d9329513b87368ff49
Arjun Murugan <arjunmcbe@gmail.com>
2024-04-28 12:28:28 +0530

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dgf-app-83596","appId":"1:434686698760:web:d8cfe8727913d11884f8bd","storageBucket":"dgf-app-83596.appspot.com","apiKey":"AIzaSyCgBUA_oaiZ4wTxe4kk37Hs0WHL27Trhlw","authDomain":"dgf-app-83596.firebaseapp.com","messagingSenderId":"434686698760","measurementId":"G-T07YQBYCNQ"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
